c32a89d5833f49c2f6c67236475a1f13aa0d0f3f
Anees <ayomideanees2003@icloud.com>
2025-01-14 18:05:57 +0100

    finished working with components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export default function NavBar () {
+    return (
+        <header>
+        <span className="header">
+        <img className="image" src="/src/react.png"  alt="React logo" />
+        <p>ReactFacts</p>
+        </span>
+        <nav>
+          <ul className="nav-list">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { Fragment } from "react";
-import Header from "./Header";
-import Main from "./MainContent";
-import Footer from "./Footer";
+// import Header from "./Header";
+// import Main from "./MainContent";
+// import Footer from "./Footer";
+import App from "./app"
 
 const root = createRoot(document.getElementById("root"));
-
-function Here() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
-}
-
-root.render(<Here />);
+root.render(<App />);
